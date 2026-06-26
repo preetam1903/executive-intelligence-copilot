3a8b90b5b1cb5e955239692c3789affa95bc36e4
preetam1903 <preetam.uce@gmail.com>
2026-06-26 10:49:31 +0530

    Add files via upload

--- chart_detector.py
+++ chart_detector.py
@@ -34,42 +34,42 @@ class ChartDetector:
 
     def detect_chart_regions(self, page_image):
 
-    image = np.array(page_image)
+        image = np.array(page_image)
 
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+        blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
-    edges = cv2.Canny(blur, 50, 150)
+        edges = cv2.Canny(blur, 50, 150)
 
-    contours, _ = cv2.findContours(
-        edges,
-        cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE
-    )
+        contours, _ = cv2.findContours(
+            edges,
+            cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE
+        )
 
-    charts = []
+        charts = []
 
-    for contour in contours:
+        for contour in contours:
 
-        x, y, w, h = cv2.boundingRect(contour)
+            x, y, w, h = cv2.boundingRect(contour)
 
-        area = w * h
+            area = w * h
 
-        if area < 80000:
-            continue
+            if area < 80000:
+                continue
 
-        charts.append({
+            charts.append({
 
-            "bbox": (x, y, w, h),
+                "bbox": (x, y, w, h),
 
-            "area": area
+                "area": area
 
-        })
+            })
 
-    charts = sorted(
-        charts,
-        key=lambda c: (c["bbox"][1], c["bbox"][0])
-    )
+        charts = sorted(
+            charts,
+            key=lambda c: (c["bbox"][1], c["bbox"][0])
+        )
 
-    return charts
+        return charts
