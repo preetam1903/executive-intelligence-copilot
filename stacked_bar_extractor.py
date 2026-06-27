38f0e39b6bd0a284346dc80b6170192700c942e6
preetam1903 <preetam.uce@gmail.com>
2026-06-27 07:24:46 +0530

    Add files via upload

--- stacked_bar_extractor.py
+++ stacked_bar_extractor.py
@@ -3,49 +3,17 @@ import numpy as np
 
 class StackedBarExtractor:
 
-    def find_bar_edges(self, img, center_x, x_axis):
+    def find_bar_edges(self, center_x):
 
-        height, width = img.shape[:2]
+        """
+        Use a fixed-width window around the expected bar center.
+        This prevents the detector from merging adjacent bars.
+        """
 
-        # -------- LEFT EDGE --------
+        BAR_HALF_WIDTH = 6
 
-        left = center_x
-
-        while left > 5:
-
-            dark = 0
-
-            for y in range(max(0, x_axis - 80), x_axis):
-
-                r, g, b = img[y, left]
-
-                if not (r > 240 and g > 240 and b > 240):
-                    dark += 1
-
-            if dark < 3:
-                break
-
-            left -= 1
-
-        # -------- RIGHT EDGE --------
-
-        right = center_x
-
-        while right < width - 5:
-
-            dark = 0
-
-            for y in range(max(0, x_axis - 80), x_axis):
-
-                r, g, b = img[y, right]
-
-                if not (r > 240 and g > 240 and b > 240):
-                    dark += 1
-
-            if dark < 3:
-                break
-
-            right += 1
+        left = max(0, center_x - BAR_HALF_WIDTH)
+        right = center_x + BAR_HALF_WIDTH
 
         return left, right
 
@@ -53,39 +21,41 @@ class StackedBarExtractor:
 
         img = np.array(pil_image)
 
-        left, right = self.find_bar_edges(
-            img,
-            center_x,
-            x_axis
-        )
+        height, width = img.shape[:2]
+
+        left, right = self.find_bar_edges(center_x)
+
+        right = min(right, width - 1)
 
         top = x_axis
 
+        found = False
+
+        # Scan upward from X-axis
         for y in range(x_axis, 0, -1):
 
-            coloured = 0
+            coloured_pixels = 0
 
-            for x in range(left, right):
+            for x in range(left, right + 1):
 
                 r, g, b = img[y, x]
 
+                # Ignore near-white pixels
                 if not (r > 240 and g > 240 and b > 240):
-                    coloured += 1
+                    coloured_pixels += 1
 
-            if coloured > (right - left) * 0.50:
+            # At least half the pixels in this row must belong to the bar
+            if coloured_pixels >= ((right - left + 1) * 0.5):
 
                 top = y
+                found = True
 
-            elif top != x_axis:
-
+            elif found:
                 break
 
         return {
-
             "left": left,
-
             "right": right,
-
+            "top": top,
             "height": x_axis - top
-
         }
